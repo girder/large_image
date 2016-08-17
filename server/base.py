6587530e97f3b7edd7a8204e5cb76ddd6b49eb5e
David Manthey <david.manthey@kitware.com>
2016-08-17 09:28:00 -0400

    Add ndpi as a file extension that is checked for large images.
    Only use the last file extension.

--- server/base.py
+++ server/base.py
@@ -57,10 +57,9 @@ def checkForLargeImageFiles(event):
     if mimeType in ('image/tiff', 'image/x-tiff', 'image/x-ptif'):
         possible = True
     exts = file.get('exts')
-    if isinstance(exts, list):
-        for ext in exts:
-            if ext in ('svs', 'ptif', 'tif', 'tiff'):
-                possible = True
+    if isinstance(exts, list) and exts[-1] in (
+            'svs', 'ptif', 'tif', 'tiff', 'ndpi'):
+        possible = True
     if not file.get('itemId') or not possible:
         return
     item = ModelImporter.model('item').load(
