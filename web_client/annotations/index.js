7ab1b3f4827b21beee0f00ff8bdf6a627bcb931f
Jonathan Beezley <jonathan.beezley@kitware.com>
2017-03-20 10:50:29 -0400

    Expose defaults object from the toplevel index

--- web_client/annotations/index.js
+++ web_client/annotations/index.js
@@ -2,10 +2,12 @@ import convert from './convert';
 import rotate from './rotate';
 import style from './style';
 import * as geometry from './geometry';
+import * as defaults from './defaults';
 
 export {
     convert,
     rotate,
     style,
-    geometry
+    geometry,
+    defaults
 };
