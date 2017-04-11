f21d1a3a179d3d348364af1e289a0414588cc898
Jonathan Beezley <jonathan.beezley@kitware.com>
2017-04-11 13:30:10 -0400

    Add an element model and collection

--- web_client/models/AnnotationModel.js
+++ web_client/models/AnnotationModel.js
@@ -1,9 +1,30 @@
+import _ from 'underscore';
 import Model from 'girder/models/Model';
 
+import ElementCollection from '../collections/ElementCollection';
 import convert from '../annotations/convert';
 
 export default Model.extend({
     resourceName: 'annotation',
+
+    defaults: {
+        'annotation': {}
+    },
+
+    initialize() {
+        this._elements = new ElementCollection(
+            this.get('annotation').elements || []
+        );
+        this._elements.annotation = this;
+
+        this.listenTo(this._elements, 'change add remove', () => {
+            // copy the object to ensure a change event is triggered
+            var annotation = _.extend({}, this.get('annotation'));
+
+            annotation.elements = this._elements.toJSON();
+            this.set('annotation', annotation);
+        });
+    },
 
     /**
      * Return the annotation as a geojson FeatureCollection.
@@ -15,5 +36,12 @@ export default Model.extend({
         const json = this.get('annotation') || {};
         const elements = json.elements || [];
         return convert(elements);
+    },
+
+    /**
+     * Return a backbone collection containing the annotation elements.
+     */
+    elements() {
+        return this._elements;
     }
 });
