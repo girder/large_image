1807624288b60d904da7c360705493011444b62f
David Manthey <david.manthey@kitware.com>
2017-04-14 15:28:11 -0400

    Fix JPEG color-space issues in Safari
    The JPEG color-space issue in Mobile Safari now is also present in Safari.  We having nothing better than user-agent scraping to determine if we need to convert to a limited set of color spaces with JFIF headers.  Test against recent Safari and Chrome user agent strings.

--- plugin_tests/tiles_test.py
+++ plugin_tests/tiles_test.py
@@ -115,6 +115,26 @@ class LargeImageTilesTest(common.LargeImageCommonTest):
         image = self.getBody(resp, text=False)
         self.assertEqual(image[:len(common.JFIFHeader)], common.JFIFHeader)
 
+        resp = self.request(
+            path='/item/%s/tiles/zxy/0/0/0' % itemId, user=self.admin,
+            isJson=False, additionalHeaders=[(
+                'User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X '
+                '10_12_3) AppleWebKit/602.4.8 (KHTML, like Gecko) '
+                'Version/10.0.3 Safari/602.4.8')])
+        self.assertStatusOk(resp)
+        image = self.getBody(resp, text=False)
+        self.assertEqual(image[:len(common.JFIFHeader)], common.JFIFHeader)
+
+        resp = self.request(
+            path='/item/%s/tiles/zxy/0/0/0' % itemId, user=self.admin,
+            isJson=False, additionalHeaders=[(
+                'User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 '
+                'Safari/537.36')])
+        self.assertStatusOk(resp)
+        image = self.getBody(resp, text=False)
+        self.assertNotEqual(image[:len(common.JFIFHeader)], common.JFIFHeader)
+
         # Ask to make this a tile-based item again
         resp = self.request(path='/item/%s/tiles' % itemId, method='POST',
                             user=self.admin, params={'fileId': fileId})
