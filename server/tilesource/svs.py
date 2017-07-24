b79f941c48bfff6e75e46fe8b243f9553fef2d16
David Manthey <david.manthey@kitware.com>
2017-07-24 10:09:10 -0400

    Refactor the power of two check to make it clearer.

--- server/tilesource/svs.py
+++ server/tilesource/svs.py
@@ -38,6 +38,25 @@ except ImportError:
     logger.getLogger().setLevel(logger.INFO)
 
 
+def _nearPowerOfTwo(val1, val2, tolerance=0.02):
+    """
+    Check if two values are different by nearly a power of two.
+
+    :param val1: the first value to check.
+    :param val2: the second value to check.
+    :param tolerance: the maximum difference in the log2 ratio's mantissa.
+    :return: True if the valeus are nearly a power of two different from each
+        other; false otherwise.
+    """
+    # If one or more of the values is zero or they have different signs, then
+    # return False
+    if val1 * val2 <= 0:
+        return False
+    log2ratio = math.log(float(val1) / float(val2)) / math.log(2)
+    # Use modf()[0] to get the mantissa of the ratio's log2 value.
+    return abs(math.modf(log2ratio)[0]) < tolerance
+
+
 @six.add_metaclass(LruCacheMetaclass)
 class SVSFileTileSource(FileTileSource):
     """
@@ -186,10 +205,8 @@ class SVSFileTileSource(FileTileSource):
         # Discard levels that are not a power-of-two compared to the highest
         # resolution level.
         levels = [entry for entry in levels if
-                  abs(((math.log(float(levels[0]['width']) / entry['width']) /
-                        math.log(2) - 0.5) % 1) - 0.5) < 0.02 and
-                  abs(((math.log(float(levels[0]['height']) / entry['height']) /
-                        math.log(2) - 0.5) % 1) - 0.5) < 0.02]
+                  _nearPowerOfTwo(levels[0]['width'], entry['width']) and
+                  _nearPowerOfTwo(levels[0]['height'], entry['height'])]
         return levels
 
     def getNativeMagnification(self):
