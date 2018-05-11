ebcc40c6b14995878bac700d95ce2d433815b61e
David Manthey <david.manthey@kitware.com>
2018-05-11 15:33:01 -0400

    Add mrxs to the list of extensions.
    If a file with this extension is imported it will automatically be
    tested to see if it can be used as a large image.

--- server/base.py
+++ server/base.py
@@ -124,7 +124,7 @@ def checkForLargeImageFiles(event):
     if mimeType in ('image/tiff', 'image/x-tiff', 'image/x-ptif'):
         possible = True
     exts = file.get('exts')
-    if exts and exts[-1] in ('svs', 'ptif', 'tif', 'tiff', 'ndpi'):
+    if exts and exts[-1] in ('svs', 'ptif', 'tif', 'tiff', 'ndpi', 'mrxs'):
         possible = True
     if not file.get('itemId') or not possible:
         return
