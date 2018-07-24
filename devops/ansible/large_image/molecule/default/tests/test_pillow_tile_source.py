283dd70955244690951a71b1fc5f0f4583d6ec24
Doruk Ozturk <doruk.ozturk@kitware.com>
2018-07-24 08:42:18 -0400

    Add tests for pil tile source

---
+++ devops/ansible/large_image/molecule/default/tests/test_pillow_tile_source.py
@@ -0,0 +1,26 @@
+"""Test the default Pillow based tile source."""
+
+import os
+import large_image
+import testinfra.utils.ansible_runner
+import urllib
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_pip_packages(host):
+    """Check python dependencies for pillow tile source."""
+    virtualenv = '~/.virtualenvs/large_image/bin/pip'
+    packages = host.pip_package.get_packages(pip_path=virtualenv).keys()
+    assert 'Pillow' in packages
+
+
+def test_pillow_tile_source():
+    """Check whether large_image can return a tile with Pillow source."""
+    test_url = 'https://data.kitware.com/api/v1/item/590346fe8d777f16d01e0546/download'  # noqa: E501
+    test_png = '/tmp/Easy1.png'
+    urllib.urlretrieve(test_url, test_png)
+    image = large_image.getTileSource(test_png)
+    # Make sure we can get a tile without an exception
+    assert type(image.getTile(0, 0, 0)) == str
