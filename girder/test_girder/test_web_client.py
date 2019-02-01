615a4ab6162439ef9b8adb6c64bc6c3b6d7cf0dc
David Manthey <david.manthey@kitware.com>
2019-02-01 13:01:43 -0500

    Don't use setup_database.  Collect javascript coverage.

--- girder/test_girder/test_web_client.py
+++ girder/test_girder/test_web_client.py
@@ -1,14 +1,12 @@
 import os
 import pytest
-import warnings
 from pytest_girder.web_client import runWebClientTest
 
+from girder.models.folder import Folder
+from girder.models.item import Item
+from girder.models.user import User
+
 from .girder_utilities import girderWorker  # noqa
-
-
-with warnings.catch_warnings():
-    warnings.filterwarnings('ignore', 'setup_database.*')
-    from pytest_girder.setup_database import setupDatabase
 
 
 @pytest.mark.usefixtures('girderWorker')  # noqa
@@ -27,13 +25,25 @@ def testWebClient(boundServer, fsAssetstore, db, spec, girderWorker):
 @pytest.mark.plugin('large_image_annotation')
 @pytest.mark.parametrize('spec', (
     'annotationListSpec.js',
-    'annotationSpec.js',
     'geojsAnnotationSpec.js',
     'geojsSpec.js',
 ))
 def testWebClientWithAnnotation(boundServer, fsAssetstore, db, spec, girderWorker):
     spec = os.path.join(os.path.dirname(__file__), 'web_client_specs', spec)
-    setupFile = os.path.splitext(spec)[0] + '.yml'
-    if os.path.exists(setupFile):
-        setupDatabase(setupFile)
+    runWebClientTest(boundServer, spec)
+
+
+@pytest.mark.usefixtures('girderWorker')  # noqa
+@pytest.mark.plugin('large_image')
+@pytest.mark.plugin('large_image_annotation')
+def testWebClientAnnotationSpec(boundServer, fsAssetstore, db, girderWorker):
+    # Create an admin user with folders and an item in the Public folder
+    user = User().createUser('admin', 'adminpassword!', 'Admin', 'Admin', 'admin@email.com', True)
+    publicFolder = next(
+        folder for folder in Folder().childFolders(parent=user, parentType='user', user=user)
+        if folder['public'] is True)
+    Item().createItem('Empty', user, publicFolder)
+
+    spec = os.path.join(os.path.dirname(__file__), 'web_client_specs', 'annotationSpec.js')
+
     runWebClientTest(boundServer, spec)
