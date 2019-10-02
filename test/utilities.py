0e32ec14944d630727ff9ee2086d67eac8d71efa
David Manthey <david.manthey@kitware.com>
2019-10-02 14:10:02 -0400

    Verify hash values of external data used in tests.

--- test/utilities.py
+++ test/utilities.py
@@ -1,3 +1,4 @@
+import hashlib
 import math
 import os
 import pytest
@@ -10,10 +11,49 @@ JPEGHeader = b'\xff\xd8\xff'
 PNGHeader = b'\x89PNG'
 TIFFHeader = b'II\x2a\x00'
 
+_checkedPaths = {}
+
+
+def deleteIfWrongHash(destpath, hashvalue):
+    """
+    Check if a file at a path has a particular sha512 hash.  If not, delete it.
+    If the file has been checked once, don't check it again.
+
+    :param destpath: the file path.
+    :param hashvalue: the sha512 hash hexdigest.
+    """
+    if os.path.exists(destpath) and destpath not in _checkedPaths and hashvalue:
+        sha512 = hashlib.sha512()
+        with open(destpath, 'rb') as f:
+            while True:
+                data = f.read(1024 * 1024)
+                if not data:
+                    break
+                sha512.update(data)
+        if sha512.hexdigest() != hashvalue:
+            os.unlink(destpath)
+        else:
+            _checkedPaths[destpath] = True
+
 
 def externaldata(
         hashpath=None, hashvalue=None, destdir='externaldata', destname=None,
         sources='https://data.kitware.com/api/v1/file/hashsum/sha512/{hashvalue}/download'):
+    """
+    Get a file from an external data source.  If the file has already been
+    downloaded, check that it has the correct hash.
+
+    :param hashpath: an optional path to a file that contains the hash value.
+        There may be white space before or after the hashvalue.
+    :param hashvalue: if no hashpath is specified, use this as a hash value.
+    :param destdir: the location to store downloaded files.
+    :param destname: if specified, the name of the file.  If hashpath is used
+        and this is None, the basename of the hashpath is used for the
+        destination name.
+    :param sources: a string or list of strings that are url templates.
+        `{hashvalue}` is replaced with the hashvalue.
+    :returns: the path to the downloaded file.
+    """
     if isinstance(sources, six.string_types):
         sources = [sources]
     curDir = os.path.dirname(os.path.realpath(__file__))
@@ -23,6 +63,7 @@ def externaldata(
             destname = os.path.splitext(os.path.basename(hashpath))[0]
     realdestdir = os.path.join(os.environ.get('TOX_WORK_DIR', curDir), destdir)
     destpath = os.path.join(realdestdir, destname)
+    deleteIfWrongHash(destpath, hashvalue)
     if not os.path.exists(destpath):
         for source in sources:
             try:
@@ -30,10 +71,14 @@ def externaldata(
                 request.raise_for_status()
                 if not os.path.exists(realdestdir):
                     os.makedirs(realdestdir)
+                sha512 = hashlib.sha512()
                 with open(destpath, 'wb') as out:
                     for buf in request.iter_content(65536):
                         out.write(buf)
+                        sha512.update(buf)
                 if os.path.getsize(destpath) == int(request.headers['content-length']):
+                    if hashvalue and sha512.hexdigest() != hashvalue:
+                        raise Exception('Download has wrong hash value - %s' % destpath)
                     break
                 raise Exception('Incomplete download (got %d of %d) of %s' % (
                     os.path.getsize(destpath), int(request.headers['content-length'], destpath)))
