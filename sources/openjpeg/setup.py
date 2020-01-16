bba7150636008803196bb7c8fb00a11225a77f62
David Manthey <david.manthey@kitware.com>
2020-01-15 23:31:01 -0500

    Pin Glymur until issues are resolved.

--- sources/openjpeg/setup.py
+++ sources/openjpeg/setup.py
@@ -42,8 +42,7 @@ setup(
     ],
     install_requires=[
         'large-image>=1.0.0',
-        'glymur>=0.8.18,<0.9 ; python_version < "3.6"',
-        'glymur>=0.8.18 ; python_version >= "3.6"',
+        'glymur>=0.8.18,<0.9',
     ],
     extras_require={
         'girder': 'girder-large-image>=1.0.0',
