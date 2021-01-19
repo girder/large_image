c5c2e50c99a4f27f12f406a30fc0c749063fd7e1
David Manthey <david.manthey@kitware.com>
2021-01-19 14:44:36 -0500

    Revert PR #520.

--- .circleci/release_pypi.sh
+++ .circleci/release_pypi.sh
@@ -5,81 +5,50 @@ set -e
 ROOTPATH=`pwd`
 
 python setup.py sdist
-pip wheel . --no-deps -w dist
 twine upload --verbose dist/*
 cd "$ROOTPATH/girder"
 python setup.py sdist
-cp "$ROOTPATH/README.rst" .
-pip wheel . --no-deps -w dist
 twine upload --verbose dist/*
 cd "$ROOTPATH/girder_annotation"
 python setup.py sdist
-cp "$ROOTPATH/README.rst" .
-pip wheel . --no-deps -w dist
 twine upload --verbose dist/*
 cd "$ROOTPATH/utilities/converter"
 python setup.py sdist
-cp "$ROOTPATH/README.rst" .
-pip wheel . --no-deps -w dist
 twine upload --verbose dist/*
 cd "$ROOTPATH/utilities/tasks"
 python setup.py sdist
-cp "$ROOTPATH/README.rst" .
-pip wheel . --no-deps -w dist
 twine upload --verbose dist/*
 cd "$ROOTPATH/sources/bioformats"
 python setup.py sdist
-cp "$ROOTPATH/README.rst" .
-pip wheel . --no-deps -w dist
 twine upload --verbose dist/*
 cd "$ROOTPATH/sources/dummy"
 python setup.py sdist
-cp "$ROOTPATH/README.rst" .
-pip wheel . --no-deps -w dist
 twine upload --verbose dist/*
 cd "$ROOTPATH/sources/gdal"
 python setup.py sdist
-cp "$ROOTPATH/README.rst" .
-pip wheel . --no-deps -w dist
 twine upload --verbose dist/*
 cd "$ROOTPATH/sources/mapnik"
 python setup.py sdist
-cp "$ROOTPATH/README.rst" .
-pip wheel . --no-deps -w dist
 twine upload --verbose dist/*
 cd "$ROOTPATH/sources/nd2"
 python setup.py sdist
-cp "$ROOTPATH/README.rst" .
-pip wheel . --no-deps -w dist
 twine upload --verbose dist/*
 cd "$ROOTPATH/sources/ometiff"
 python setup.py sdist
-cp "$ROOTPATH/README.rst" .
-pip wheel . --no-deps -w dist
 twine upload --verbose dist/*
 cd "$ROOTPATH/sources/openjpeg"
 python setup.py sdist
-cp "$ROOTPATH/README.rst" .
-pip wheel . --no-deps -w dist
 twine upload --verbose dist/*
 cd "$ROOTPATH/sources/openslide"
 python setup.py sdist
-cp "$ROOTPATH/README.rst" .
-pip wheel . --no-deps -w dist
 twine upload --verbose dist/*
 cd "$ROOTPATH/sources/pil"
 python setup.py sdist
-cp "$ROOTPATH/README.rst" .
-pip wheel . --no-deps -w dist
 twine upload --verbose dist/*
 cd "$ROOTPATH/sources/test"
 python setup.py sdist
-cp "$ROOTPATH/README.rst" .
-pip wheel . --no-deps -w dist
 twine upload --verbose dist/*
 cd "$ROOTPATH/sources/tiff"
 python setup.py sdist
-cp "$ROOTPATH/README.rst" .
-pip wheel . --no-deps -w dist
 twine upload --verbose dist/*
 
