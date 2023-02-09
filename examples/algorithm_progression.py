cb8a02c8267e98ac374a0599b074a222dec54f59
David Manthey <david.manthey@kitware.com>
2023-02-09 16:01:06 -0500

    Hacky sweep algorithm progression.

--- examples/algorithm_progression.py
+++ examples/algorithm_progression.py
@@ -1,42 +1,59 @@
-import algorithms
-import large_image
+import itertools
 import os
 import pathlib
+import shutil
 import sys
+
+import algorithms
+import numpy
 import yaml
 
+import large_image
 
-def apply_algorithm(algorithm, source):
+
+def apply_algorithm(algorithm, source, params):
     new_source = large_image.new()
     for tile in source.tileIterator(
         format=large_image.tilesource.TILE_FORMAT_NUMPY,
         tile_size=dict(width=2048, height=2048),
     ):
-        altered_data = algorithm(tile['tile'])
+        altered_data = algorithm(tile['tile'], **params)
         new_source.addTile(altered_data, tile['x'], tile['y'])
     return new_source
 
 
 def save_result(source, iteration, output_folder):
-    filename = f'iteration_{iteration}.tiff'
+    filename = f'iteration_{iteration if isinstance(iteration, int) else "_".join(str(v) for v in iteration)}.tiff'  # noqa
     filepath = pathlib.Path(output_folder, filename)
-    source.write(filepath, lossy=False)
+    source.write(str(filepath), lossy=False)
     return filename, iteration
 
 
 def combine_results(results, output_folder, algorithm_name, input_image):
+    base = os.path.basename(input_image)
+    shutil.copy(input_image, pathlib.Path(output_folder, base))
     yaml_dict = {
         'name': f'{algorithm_name.title()} iterative results',
-        'description': f'Saved results from {len(results)} iterations of the \
-            {algorithm_name} algorithm performed on {input_image}',
-        'sources': [{'path': input_image}] + [
+        'description':
+            f'Saved results from {len(results)} iterations of ' +
+            f'the {algorithm_name} algorithm performed on {input_image}',
+        'sources': [{'path': base}],
+    }
+    if len(results[0][1]) == 1:
+        yaml_dict['sources'] += [
             {
                 'path': r[0],
-                'z': r[1],
+                'z': r[1][0] + 1,
             }
             for r in results
-        ],
-    }
+        ]
+    else:
+        yaml_dict['sources'] = []
+        for r in results:
+            entry = {'path': r[0]}
+            for idx, key in enumerate(['z', 't', 'xy', 'c'][:len(r[1])]):
+                entry[key] = r[1][idx]
+            yaml_dict['sources'].append(entry)
     with open(pathlib.Path(output_folder, 'results.yml'), 'w') as f:
         yaml.dump(
             yaml_dict,
@@ -71,11 +88,69 @@ def iterate_algorithm(input_image, algorithm, n):
     )
 
 
+def apply_and_result(module, algname, input_image, paramlist, output_folder):
+    source = large_image.open(input_image)
+    algorithm = getattr(__import__(module), algname)
+    alparams = {entry[1]: entry[2] for entry in paramlist}
+    print(alparams)
+    output = apply_algorithm(algorithm, source, alparams)
+    result = save_result(
+        output,
+        [entry[0] for entry in paramlist],
+        output_folder,
+    )
+    return result
+
+
+def sweep_algorithm(input_image, algorithm, params):
+    """
+    See:
+
+    params = [('key1', [values1]), ('key2', [values2]), ...]
+    """
+    algorithm_name = algorithm.__name__
+    output_folder = algorithm_name + '_output'
+    if not pathlib.Path(output_folder).exists():
+        os.mkdir(output_folder)
+    print(output_folder)
+
+    lists = [
+        [(idx, param[0], val) for idx, val in enumerate(param[1])]
+        for param in params
+    ]
+    # pass the algorithm module and name here to make it possible to do this
+    # more flexibly in the future.
+    results = [
+        apply_and_result('algorithms', algorithm_name, input_image, paramlist, output_folder)
+        for paramlist in itertools.product(*lists)]
+
+    combine_results(
+        results,
+        output_folder,
+        algorithm_name,
+        input_image,
+    )
+
+
 if __name__ == '__main__':
     args = sys.argv[1:]
     filename = args[0]
+    """
     iterate_algorithm(
         filename,
         algorithms.erode,
         n=5,
+    )
+    """
+    sweep_algorithm(
+        filename,
+        algorithms.positive_pixel_count,
+        params=[
+            ('hue_value', numpy.linspace(0, 1, 20, endpoint=False)),
+            # ('hue_width', numpy.linspace(0.05, 0.25, 5)),
+            # ('hue_value', numpy.linspace(0, 1, 256, endpoint=False)),
+            ('hue_width', numpy.linspace(0.15, 0.15, 1)),
+            # ('hue_value', numpy.linspace(0, 1, 250, endpoint=False)),
+            # ('hue_width', numpy.linspace(0.05, 0.25, 9)),
+        ],
     )
--- examples/algorithms.py
+++ examples/algorithms.py
@@ -1,5 +1,5 @@
-from scipy import ndimage
 import numpy as np
+from scipy import ndimage
 
 
 def rgb2gray(rgb):
@@ -17,3 +17,63 @@ def erode(data):
     data[eroded == 0] = 0
     data[:, :, 3] = 255
     return data
+
+
+class Labels:
+    NEGATIVE = 0
+    WEAK = 1
+    PLAIN = 2
+    STRONG = 3
+
+
+def rgb_to_hsi(im):
+    """
+    Convert to H/S/I the R/G/B pixels in im.
+
+    Adapted from
+    https://en.wikipedia.org/wiki/HSL_and_HSV#Hue_and_chroma.
+    """
+    im = np.moveaxis(im, -1, 0)
+    if len(im) not in (3, 4):
+        raise ValueError('Expected 3-channel RGB or 4-channel RGBA image; '
+                         'received a {}-channel image'.format(len(im)))
+    im = im[:3]
+    hues = (np.arctan2(3**0.5 * (im[1] - im[2]),
+                       2 * im[0] - im[1] - im[2]) / (2 * np.pi)) % 1
+    intensities = im.mean(0)
+    saturations = np.where(
+        intensities, 1 - im.min(0) / np.maximum(intensities, 1e-10), 0)
+    return np.stack([hues, saturations, intensities], -1)
+
+
+def positive_pixel_count(
+        image, hue_value=0.83, hue_width=0.15, saturation_minimum=0.05,
+        intensity_upper_limit=0.95, intensity_lower_limit=0.05,
+        intensity_weak_threshold=0.65, intensity_strong_threshold=0.35):
+
+    image_hsi = rgb_to_hsi(image / 255)
+    mask_all_positive = (
+        (np.abs(((image_hsi[..., 0] - hue_value + 0.5) % 1) - 0.5) <=
+         hue_width / 2) &
+        (image_hsi[..., 1] >= saturation_minimum) &
+        (image_hsi[..., 2] < intensity_upper_limit) &
+        (image_hsi[..., 2] >= intensity_lower_limit)
+    )
+    all_positive_i = image_hsi[mask_all_positive, 2]
+    mask_weak = all_positive_i >= intensity_weak_threshold
+    mask_strong = all_positive_i < intensity_strong_threshold
+    mask_pos = ~(mask_weak | mask_strong)
+
+    label_image = np.full(image.shape[:-1], Labels.NEGATIVE, dtype=np.uint8)
+    label_image[mask_all_positive] = (
+        mask_weak * Labels.WEAK +
+        mask_pos * Labels.PLAIN +
+        mask_strong * Labels.STRONG
+    )
+    color_map = np.empty((4, 4), dtype=np.uint8)
+    color_map[Labels.NEGATIVE] = 255, 255, 255, 255
+    color_map[Labels.WEAK] = 60, 78, 194, 255
+    color_map[Labels.PLAIN] = 221, 220, 220, 255
+    color_map[Labels.STRONG] = 180, 4, 38, 255
+    ppcimg = color_map[label_image]
+    return ppcimg
