f6915e42958526076b8ce51ab81943b926a0aca2
Anne Haley <anne.haley@kitware.com>
2023-03-08 23:39:37 -0500

    Use zarr storage with xarray method

--- examples/algorithm_progression/xarray_sweep.py
+++ examples/algorithm_progression/xarray_sweep.py
@@ -8,62 +8,79 @@ import large_image
 
 
 class AlgorithmSweepXArray(AlgorithmSweep):
+    def create_ds(self):
+        dim_sizes = dict(
+            **{k: len(v) for k, v in self.input_params.items()},
+            **{
+                "y": self.source_metadata["sizeY"],
+                "x": self.source_metadata["sizeX"],
+                "c": 4,
+            },
+        )
+
+        ds = xr.Dataset(
+            {
+                "results": (
+                    list(dim_sizes.keys()),
+                    np.zeros(
+                        tuple(dim_sizes.values()),
+                    ),
+                )
+            },
+            coords=self.input_params,
+            attrs={
+                "name": f"{self.algorithm_name} iterative results",
+                "description": f"{self.algorithm_name} algorithm performed on {self.input_filename}",
+            },
+        )
+        return ds
+
     def initialize_storage(self):
-        self.output_files = []
+        self.source = large_image.open(self.input_filename)
+        self.source_metadata = self.source.getMetadata()
+        self.zarr_path = Path(self.output_dir, "results.zarr")
+        ds = self.create_ds()
+        ds.to_zarr(self.zarr_path, mode="w", compute=False)
 
     def apply_algorithm(self, param_combo):
-        iteration_filename = (
-            Path(self.output_dir) / f'result_{"_".join(str(v) for v in param_combo)}.nc'
-        )
-        if not iteration_filename.exists():
-            current_coords = {
-                k: [param_combo[i]] for i, k in enumerate(self.input_params.keys())
-            }
-            source = large_image.open(self.input_filename)
-            source_metadata = source.getMetadata()
-            output_data = np.empty(
-                (
-                    source_metadata["sizeY"],
-                    source_metadata["sizeX"],
-                    4,
-                )
+        current_coords = {
+            k: [param_combo[i]] for i, k in enumerate(self.input_params.keys())
+        }
+        output_data = np.empty(
+            (
+                self.source_metadata["sizeY"],
+                self.source_metadata["sizeX"],
+                4,
             )
-            for tile in source.tileIterator(
-                format=large_image.tilesource.TILE_FORMAT_NUMPY,
-                tile_size=dict(width=2048, height=2048),
-            ):
-                output_data[
-                    tile["y"]: tile["y"] + tile["height"],
-                    tile["x"]: tile["x"] + tile["width"],
-                ] = self.algorithm(tile["tile"], *param_combo)
+        )
+        for tile in self.source.tileIterator(
+            format=large_image.tilesource.TILE_FORMAT_NUMPY,
+            tile_size=dict(width=2048, height=2048),
+        ):
+            output_data[
+                tile["y"]: tile["y"] + tile["height"],
+                tile["x"]: tile["x"] + tile["width"],
+            ] = self.algorithm(tile["tile"], *param_combo)
 
-            xr.DataArray(
-                output_data,
-                dims=["y", "x", "c"],
-                attrs=current_coords,
-            ).to_netcdf(iteration_filename)
-        return iteration_filename
+        output_ds = self.create_ds()
+        output_ds["results"].loc[current_coords] = output_data
+
+        current_index_region = {
+            k: list(v.values).index(current_coords[k][0])
+            for k, v in output_ds["results"].coords.items()
+        }
+        current_index_region_slice = {
+            k: slice(i, i + 1) for k, i in current_index_region.items()
+        }
+        output_ds.isel(current_index_region_slice).to_zarr(
+            self.zarr_path,
+            mode="a",
+            region=current_index_region_slice,
+        )
 
     def handle_result(self, result):
-        self.output_files.append(result)
+        pass
 
     def complete_storage(self):
-        if len(self.output_files) == 0:
-            return None
-
-        first_result = xr.open_dataarray(self.output_files[0])
-        dims = dict(
-            **{k: len(v) for k, v in self.input_params.items()},
-            **first_result.sizes,
-        )
-        composite = xr.DataArray(
-            np.empty(tuple(dims.values())),
-            coords=self.input_params,
-            dims=dims,
-        )
-
-        for filename in self.output_files:
-            result = xr.open_dataarray(filename)
-            composite.loc[result.attrs] = result.astype(int)
-            filename.unlink()
-        composite.to_netcdf(Path(self.output_dir, "results.nc"))
+        ds = xr.open_zarr(self.zarr_path)
+        print(ds)
