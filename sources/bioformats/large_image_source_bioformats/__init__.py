5995a790a1cfe6ae279e44d8b6769b18ba8ccfce
David Manthey <david.manthey@kitware.com>
2023-10-03 16:48:48 -0400

    Prohibit bioformats and vips from reading mrxs directly.

--- sources/bioformats/large_image_source_bioformats/__init__.py
+++ sources/bioformats/large_image_source_bioformats/__init__.py
@@ -62,7 +62,7 @@ _openImages = []
 
 # Default to ignoring files with no extension and some specific extensions.
 config.ConfigValues['source_bioformats_ignored_names'] = \
-    r'(^[^.]*|\.(jpg|jpeg|jpe|png|tif|tiff|ndpi|nd2|ome|nc|json|isyntax))$'
+    r'(^[^.]*|\.(jpg|jpeg|jpe|png|tif|tiff|ndpi|nd2|ome|nc|json|isyntax|mrxs))$'
 
 
 def _monitor_thread():
