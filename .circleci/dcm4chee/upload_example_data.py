7675ca6a9dfae01d4fc4e7a74f57f68a79b4f9a1
Patrick Avery <patrick.avery@kitware.com>
2024-01-08 09:51:05 -0600

    Use hashsum downloads for sample data files
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- .circleci/dcm4chee/upload_example_data.py
+++ .circleci/dcm4chee/upload_example_data.py
@@ -10,11 +10,15 @@ from pydicom import dcmread
 def upload_example_data(server_url):
 
     # This is TCGA-AA-3697
-    download_urls = [
-        'https://data.kitware.com/api/v1/file/6594790e9c30d6f4e17c9d0d/download',
-        'https://data.kitware.com/api/v1/file/6594790f9c30d6f4e17c9d10/download',
-        'https://data.kitware.com/api/v1/file/6594790d9c30d6f4e17c9d0a/download',
+    sha512s = [
+        '48cb562b94d0daf4060abd9eef150c851d3509d9abbff4bea11d00832955720bf1941073a51e6fb68fb5cc23704dec2659fc0c02360a8ac753dc523dca2c8c36', # noqa
+        '36432183380eb7d44417a2210a19d550527abd1181255e19ed5c1d17695d8bb8ca42f5b426a63fa73b84e0e17b770401a377ae0c705d0ed7fdf30d571ef60e2d', # noqa
+        '99bd3da4b8e11ce7b4f7ed8a294ed0c37437320667a06c40c383f4b29be85fe8e6094043e0600bee0ba879f2401de4c57285800a4a23da2caf2eb94e5b847ee0', # noqa
     ]
+    download_urls = [
+        f'https://data.kitware.com/api/v1/file/hashsum/sha512/{x}/download' for x in sha512s
+    ]
+
     datasets = []
     for url in download_urls:
         resp = urllib.request.urlopen(url)
