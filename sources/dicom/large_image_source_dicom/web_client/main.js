6a21c512497f7f00e6fc847a08da6b071b979745
willdunklin <will.dunklin@kitware.com>
2024-04-10 16:21:39 -0400

    Switch DICOMweb assetstore imports to use girder generics

--- sources/dicom/large_image_source_dicom/web_client/main.js
+++ sources/dicom/large_image_source_dicom/web_client/main.js
@@ -1,7 +1,6 @@
-import './routes';
-
 // Extends and overrides API
 import './constants';
+import './views/DICOMwebImportView';
 import './views/AssetstoresView';
 import './views/EditAssetstoreWidget';
 import './views/NewAssetstoreWidget';
