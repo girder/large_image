Fix scaling small images in the multi source
PR #1627 fixed one problem and introduced another.  This fixes it.

sources/multi/large_image_source_multi/__init__.py
@@ -1119,8 +1119,8 @@ class MultiFileTileSource(FileTileSource, metaclass=LruCacheMetaclass):
         # Recompute where the source corners will land
         destcorners = (np.dot(transform, regioncorners.T).T).tolist()
         destShape = [
-            max(max(math.ceil(c[0]) for c in destcorners), srcImage.shape[1]),
             max(max(math.ceil(c[1]) for c in destcorners), srcImage.shape[0]),
+            max(max(math.ceil(c[0]) for c in destcorners), srcImage.shape[1]),
         ]
         if max(0, -x) or max(0, -y):
             transform[0][2] -= max(0, -x)

test/test_source_multi.py
@@ -274,13 +274,13 @@ def testMultiAffineTransform():
     source = large_image_source_multi.open(imagePath)
     frameval = [
         [243, 243, 49065],
-        [3924, 3924, 47676],
-        [13, 13, 49148],
-        [2592, 2592, 48093],
-        [235, 235, 49062],
-        [942, 942, 48785],
-        [1014, 1014, 48872],
-        [198, 198, 49063],
+        [3920, 3920, 47672],
+        [13, 13, 49149],
+        [3943, 3943, 47695],
+        [246, 246, 49063],
+        [972, 972, 48785],
+        [1455, 1455, 48597],
+        [204, 204, 49059],
     ]
     for frame in range(8):
         thumbs = {}
@@ -292,9 +292,9 @@ def testMultiAffineTransform():
             r = np.sum(img[:, :, 0] >= 128)
             g = np.sum(img[:, :, 1] >= 128)
             b = np.sum(img[:, :, 2] >= 128)
-            assert abs(r - frameval[frame][0]) < 250
-            assert abs(g - frameval[frame][1]) < 250
-            assert abs(b - frameval[frame][2]) < 250
+            assert abs(r - frameval[frame][0]) < 200
+            assert abs(g - frameval[frame][1]) < 200
+            assert abs(b - frameval[frame][2]) < 200
         assert abs(np.average(thumbs[2048] - thumbs[256])) < 7
         assert abs(np.average(thumbs[1024] - thumbs[256])) < 7
         assert abs(np.average(thumbs[512] - thumbs[256])) < 7