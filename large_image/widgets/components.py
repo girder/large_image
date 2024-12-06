ceb934bc8257adccc49aed9b5942fa1dfb06023c
Anne Haley <anne.haley@kitware.com>
2024-12-06 18:45:00 +0000

    Fix test failures

--- large_image/widgets/components.py
+++ large_image/widgets/components.py
@@ -1,18 +1,19 @@
 import ipyvue
 import traitlets
 from pathlib import Path
+from typing import Callable, List, Union
 
 
-class DualInput(ipyvue.VueTemplate):
+class DualInput(ipyvue.VueTemplate):  # type: ignore
     template_file = __file__, 'DualInput.vue'
 
     label = traitlets.Unicode(default_value='Value').tag(sync=True)
     currentValue = traitlets.Int(default_value=0).tag(sync=True)
     valueMax = traitlets.Int(default_value=10).tag(sync=True)
-    sliderLabels = traitlets.List(default_value=[]).tag(sync=True)
+    sliderLabels = traitlets.List([""], allow_none=True).tag(sync=True)
     maxMerge = traitlets.Bool(default_value=False).tag(sync=True)
 
-class FrameSelector(ipyvue.VueTemplate, traitlets.HasTraits):
+class FrameSelector(ipyvue.VueTemplate ):  # type: ignore
     template_file = __file__, 'FrameSelector.vue'
     components = traitlets.Dict({
         'DualInput': DualInput().template.template,
@@ -20,7 +21,7 @@ class FrameSelector(ipyvue.VueTemplate, traitlets.HasTraits):
 
     imageMetadata = traitlets.Dict().tag(sync=True)
     currentFrame = traitlets.Int(default_value=0).tag(sync=True)
-    updateFrameCallback = None
+    updateFrameCallback: Union[Callable, None] = None
 
     def vue_frameUpdate(self, data=None):
         if self.updateFrameCallback is not None:
