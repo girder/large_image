40382c7b78c1a570f3e9598a690d4404b208bbbd
Anne Haley <anne.haley@kitware.com>
2025-01-14 19:33:49 +0000

    Remove unnecessary `elif` after `return`

--- large_image/tilesource/jupyter.py
+++ large_image/tilesource/jupyter.py
@@ -441,9 +441,9 @@ class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.integer):
             return int(obj)
-        elif isinstance(obj, np.floating):
+        if isinstance(obj, np.floating):
             return float(obj)
-        elif isinstance(obj, np.ndarray):
+        if isinstance(obj, np.ndarray):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
 
