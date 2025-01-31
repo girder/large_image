a71bc8926449bde011884ebd4e81f25b9197d53e
naglepuff <mike.nagler@kitware.com>
2025-01-31 16:24:28 -0500

    Initial girder 5 conversion

--- sources/dicom/large_image_source_dicom/web_client/views/DICOMwebImportView.js
+++ sources/dicom/large_image_source_dicom/web_client/views/DICOMwebImportView.js
@@ -1,14 +1,12 @@
-import $ from 'jquery';
-
-import BrowserWidget from '@girder/core/views/widgets/BrowserWidget';
-import router from '@girder/core/router';
-import View from '@girder/core/views/View';
-import { restRequest } from '@girder/core/rest';
-
-import { assetstoreImportViewMap } from '@girder/core/views/body/AssetstoresView';
-import { AssetstoreType } from '@girder/core/constants';
-
 import DWASImportTemplate from '../templates/assetstoreImport.pug';
+
+const $ = girder.$;
+const BrowserWidget = girder.views.widgets.BrowserWidget;
+const router = girder.router;
+const View = girder.views;
+const { restRequest } = girder.rest;
+const { assetstoreImportViewMap } = girder.views.body.AssetstoresView;
+const { AssetstoreType } = girder.constants;
 
 const DICOMwebImportView = View.extend({
     events: {
