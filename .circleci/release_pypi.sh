161b610f3da4f75464090f477ca5a0fc2eb53e99
David Manthey <david.manthey@kitware.com>
2025-04-08 11:34:22 -0400

    Conform to a standard SPDX license expression

--- .circleci/release_pypi.sh
+++ .circleci/release_pypi.sh
@@ -7,8 +7,9 @@ ROOTPATH=`pwd`
 export SETUPTOOLS_SCM_PRETEND_VERSION=`python -m setuptools_scm | sed "s/.* //"`
 if [ ${CIRCLE_BRANCH-:} = "master" ]; then
     export SETUPTOOLS_SCM_PRETEND_VERSION=`echo $SETUPTOOLS_SCM_PRETEND_VERSION | sed "s/\+.*$//"`
-elif [ "${CIRCLE_BRANCH}" == "girder-5" ]; then
-    export SETUPTOOLS_SCM_PRETEND_VERSION=$(python -c "import re; print(re.sub(r'(\d+\.\d+\.\d+)dev(\d+)', r'\1a\2', get_version()))")
+elif [ "${CIRCLE_BRANCH-:}" == "girder-5" ]; then
+    pip install setuptools-scm
+    export SETUPTOOLS_SCM_PRETEND_VERSION=$(python -c "import setuptools_scm,re;print(re.sub(r'(\d+\.\d+\.\d+\.)dev(\d+)(\+.*$)', r'\1a\2', setuptools_scm.get_version()))")
 fi
 
 python setup.py sdist
